335e5c0638a9f193ef5cab15d5855d7fec67e784
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2025-04-17 15:39:53 +0200

    *Request file

--- core/Request.php
+++ core/Request.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace Core;
+
+class Request{
+    private array $data;
+    private array $server;
+
+    public function __construct(array $data, array $server){
+        $this->data = $data;
+        $this->server = $server;
+    }
+
+    public function get(string $key): ?string{
+        return $this->data[$key] ?? null;
+    }
+
+    public function getIp(): string{
+        return $this->server['REMOTE_ADDR'] ?? "unknown";
+    }
+}
