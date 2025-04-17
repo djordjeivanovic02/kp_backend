d82ca72402d662df0930433e918ad7432b18260c
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2025-04-17 15:41:29 +0200

    *Response file

--- core/Response.php
+++ core/Response.php
@@ -1,1 +1,12 @@
 <?php
+
+namespace Core;
+
+class Response{
+    public function json(array $data): void
+    {
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    }
+}
