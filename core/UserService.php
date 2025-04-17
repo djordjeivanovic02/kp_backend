0d9393e241751f33149bd6524bd6046611f3fea6
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2025-04-17 17:15:59 +0200

    *Autoload

--- core/UserService.php
+++ core/UserService.php
@@ -1,1 +1,34 @@
 <?php
+
+namespace Core;
+
+use Core\Validation\ValidatorInterface;
+
+class UserService{
+    private array $validators;
+    private UserRepository $repo;
+    private Mailer $mailer;
+
+    public function __construct(array $validators, UserRepository $repo, Mailer $mailer){
+        $this->validators = $validators;
+        $this->repo = $repo;
+        $this->mailer = $mailer;
+    }
+
+    public function register(string $email, string $password, string $password2, string $ip): array{
+        $data = compact('email', 'password', 'password2', 'ip');
+        foreach ($this->validators as $validator) {
+            $validator->validate($data);
+        }
+        $userId = $this->repo->insert([
+            'email' => $email,
+            'password' => $password,
+            'posted' => 'NOW()'
+        ]);
+
+        $this->mailer->sendWelcomeEmail($email);
+        $this->repo->insertLog($userId);
+
+        return ['id' => $userId];
+    }
+}
